186e4500dd8176afadd911b65a067f0e98b164c6
taarema8 <marius.reusch@swisscom.com>
2018-01-11 08:49:40 +0100

    add sample greeting to db

--- src/main/java/com/swisscom/iam/sit/SampleDataInitializr.java
+++ src/main/java/com/swisscom/iam/sit/SampleDataInitializr.java
@@ -1,5 +1,7 @@
 package com.swisscom.iam.sit;
 
+import com.swisscom.iam.sit.domain.Greeting;
+import com.swisscom.iam.sit.repository.GreetingRepository;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -7,9 +9,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleDataInitializr implements ApplicationRunner {
 
+    private final GreetingRepository greetingRepository;
+
+    public SampleDataInitializr(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
 
     @Override
     public void run(ApplicationArguments args) {
-
+        greetingRepository.save(new Greeting("Na du", "Lappen"));
     }
 }
--- src/main/java/com/swisscom/iam/sit/repository/GreetingRepository.java
+++ src/main/java/com/swisscom/iam/sit/repository/GreetingRepository.java
@@ -1,6 +1,5 @@
 package com.swisscom.iam.sit.repository;
 
-import com.swisscom.iam.sit.GreetApp;
 import com.swisscom.iam.sit.domain.Greeting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
